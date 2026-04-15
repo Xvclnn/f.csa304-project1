a094ed8727c815406beee467d655b371509251e3
Xvclnn <shinshinpachi1030@gmail.com>
2026-04-16 02:51:42 +0800

    minii shit

--- src/simulation.py
+++ src/simulation.py
@@ -9,8 +9,8 @@ def calc_density(h, constant_density):
 def get_drag_params(t, t_shuher_zadrah):
     """Хугацаанаас хамаарч шүхрийн параметрүүдийг буцаах."""
     if t < t_shuher_zadrah:
-        return 0.7, 0.5  # C, A (Шүхэргүй)
-    return 1.5, 30.0     # C, A (Шүхэртэй)
+        return 0.7, 0.5  # C, A (Шүхэр дэлгээгүй)
+    return 1.5, 30.0     # C, A (Шүхэр дэлгэсэн)
 
 def newton_2nd(v, h, t, m, t_shuher_zadrah, constant_density):
     """Хурд болон өндрийн уламжлалыг тооцоолох."""
@@ -18,7 +18,7 @@ def newton_2nd(v, h, t, m, t_shuher_zadrah, constant_density):
     C, A = get_drag_params(t, t_shuher_zadrah)
     D = calc_density(h, constant_density)
     
-    hurdatgal = g - (0.5 * C * D * A * v**2) / m
+    hurdatgal = g - (0.5 * C * D * A * v * abs(v)) / m
     dh_dt = -v
     return hurdatgal, dh_dt
 
@@ -32,29 +32,31 @@ def simulate_jump(m, h0, t_shuher_zadrah, dt, method, constant_density):
     v[0] = 0.0 # anhnii hurd  ugasa 0
     for i in range(max_steps - 1):
         if h[i] <= 0:
+            h[i] = 0
+            v[i] = 0
             return t[:i+1], v[:i+1], h[:i+1]  # gazart hurehed shuud utguudiig butsaana
             
         if method == 'euler':
-            dv, dh = newton_2nd(v[i], h[i], t[i], m, t_shuher_zadrah, constant_density)
-            v[i+1] = v[i] + dv * dt
+            hurdatgal, dh = newton_2nd(v[i], h[i], t[i], m, t_shuher_zadrah, constant_density)
+            v[i+1] = v[i] + hurdatgal * dt
             h[i+1] = h[i] + dh * dt
             
         elif method == 'rk4':
-            dv1, dh1 = newton_2nd(v[i], h[i], t[i], m, t_shuher_zadrah, constant_density)
+            hurdatgal1, dh1 = newton_2nd(v[i], h[i], t[i], m, t_shuher_zadrah, constant_density)
             
-            v_half1 = v[i] + 0.5 * dv1 * dt
+            v_half1 = v[i] + 0.5 * hurdatgal1 * dt
             h_half1 = h[i] + 0.5 * dh1 * dt
-            dv2, dh2 = newton_2nd(v_half1, h_half1, t[i] + 0.5 * dt, m, t_shuher_zadrah, constant_density)
+            hurdatgal2, dh2 = newton_2nd(v_half1, h_half1, t[i] + 0.5 * dt, m, t_shuher_zadrah, constant_density)
             
-            v_half2 = v[i] + 0.5 * dv2 * dt
+            v_half2 = v[i] + 0.5 * hurdatgal2 * dt
             h_half2 = h[i] + 0.5 * dh2 * dt
-            dv3, dh3 = newton_2nd(v_half2, h_half2, t[i] + 0.5 * dt, m, t_shuher_zadrah, constant_density)
+            hurdatgal3, dh3 = newton_2nd(v_half2, h_half2, t[i] + 0.5 * dt, m, t_shuher_zadrah, constant_density)
             
-            v_full = v[i] + dv3 * dt
+            v_full = v[i] + hurdatgal3 * dt
             h_full = h[i] + dh3 * dt
-            dv4, dh4 = newton_2nd(v_full, h_full, t[i] + dt, m, t_shuher_zadrah, constant_density)
+            hurdatgal, dh4 = newton_2nd(v_full, h_full, t[i] + dt, m, t_shuher_zadrah, constant_density)
             
-            v[i+1] = v[i] + (dt / 6.0) * (dv1 + 2*dv2 + 2*dv3 + dv4)
+            v[i+1] = v[i] + (dt / 6.0) * (hurdatgal1 + 2*hurdatgal2 + 2*hurdatgal3 + hurdatgal)
             h[i+1] = h[i] + (dt / 6.0) * (dh1 + 2*dh2 + 2*dh3 + dh4)
             
         t[i+1] = t[i] + dt
--- src/visualization.py
+++ src/visualization.py
@@ -2,39 +2,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 from simulation import simulate_jump
 
+methods = ['euler', 'rk4']
+masses = [60, 70, 80, 90, 100]  #kg
+initial_heights = [2000, 3000, 4000, 5000]  #meter
+t_shuher_zadrahs = [50, 60, 70] #second
+delta_t_s = [1.0, 0.1, 0.01] #second
+constant_density_songolt = [True, False] 
+
 def plot_base_simulation():
-    """Үндсэн симуляцын хурд болон өндрийн хугацаанаас хамаарсан график."""
-    t, v, h = simulate_jump(dt=0.1)
+    """
+    Үндсэн симуляцын хурд болон өндрийн хугацаанаас хамаарсан график байгуулна.
+    """
+    t, v, h = simulate_jump(m=86, h0=4000, t_shuher_zadrah=60, dt=delta_t_s[1], method='euler', constant_density=False)
     
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+    fig, (plot1, plot2) = plt.subplots(2, 1, figsize=(10, 8))
     
-    ax1.plot(t, v, 'b-', label='Хурд (м/с)')
-    ax1.axvline(x=60, color='r', linestyle='--', label='Шүхэр задрах агшин')
-    ax1.set_title('Аврагчийн унах хурд')
-    ax1.set_xlabel('Хугацаа (с)')
-    ax1.set_ylabel('Хурд (м/с)')
-    ax1.grid(True)
-    ax1.legend()
+    plot1.plot(t, v, 'b-', label='Хурд (м/с)')
+    plot1.axvline(x=60, color='r', linestyle='--', label='Шүхэр задрах агшин')
+    plot1.set_title('Унах хурдны өөрчлөлт')
+    plot1.set_xlabel('Хугацаа (с)')
+    plot1.set_ylabel('Хурд (м/с)')
+    plot1.grid(True)
+    plot1.legend()
     
-    ax2.plot(t, h, 'g-', label='Өндөр (м)')
-    ax2.axvline(x=60, color='r', linestyle='--', label='Шүхэр задрах агшин')
-    ax2.set_title('Аврагчийн өндрийн өөрчлөлт')
-    ax2.set_xlabel('Хугацаа (с)')
-    ax2.set_ylabel('Өндөр (м)')
-    ax2.grid(True)
-    ax2.legend()
+    plot2.plot(t, h, 'g-', label='Өндөр (м)')
+    plot2.axvline(x=60, color='r', linestyle='--', label='Шүхэр задрах агшин')
+    plot2.set_title('ДТД Өндрийн өөрчлөлт')
+    plot2.set_xlabel('Хугацаа (с)')
+    plot2.set_ylabel('Өндөр (м)')
+    plot2.grid(True)
+    plot2.legend()
     
     plt.tight_layout()
     plt.show()
 
 def plot_mass_analysis():
     """Төрөл бүрийн масстай үеийн буух хурдыг харьцуулах."""
-    masses = [60, 70, 80, 90, 100]
     
     plt.figure(figsize=(10, 6))
-    for m in masses:
-        t, v, h = simulate_jump(m=m)
-        plt.plot(t, v, label=f'm={m} кг')
+    for mass in masses:
+        t, v, h = simulate_jump(m=mass, h0=4000, t_shuher_zadrah=60, dt=delta_t_s[1], method='euler', constant_density=False)
+        plt.plot(t, v, label=f'm={mass} кг')
         
     plt.axvline(x=60, color='k', linestyle='--', alpha=0.5)
     plt.title('Ялгаатай масстай үеийн хурдны харьцуулалт')
